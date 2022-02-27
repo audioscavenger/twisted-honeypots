f7f74d1924afb3ab19d711c768515305030e1642
Eric <dev@derewonko.com>
2022-02-26 22:54:24 -0700

    Update vars.sh
    added missing path

--- vars.sh
+++ vars.sh
@@ -4,3 +4,4 @@ export MYSQL_PWD=password
 export TWISTED_PID=/var/run/twistd-pot.pid
 export TWISTED_LOG=/var/log/twistd-pot.log
 export PYTHONPATH=${PYTHONPATH}:/opt/twisted-honeypots/python
+export PATH=$PATH:/root/.local/bin
