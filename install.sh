c79f01c76603a4adf5a20797550e37bb1470de1b
Eric <dev@derewonko.com>
2022-02-26 23:49:17 -0700

    Update install.sh
    added TODO

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ export PATH=$PATH:/root/.local/bin
 
 apk --update --upgrade add git python3 py3-pip python3-dev curl sudo geoip libc-dev linux-headers libffi-dev openssl-dev mariadb-dev
 pip install --upgrade pip
+
+# TODO: test wheeled install: virtualenv --python=python3 -r requirements.txt --user --upgrade
 pip3 install -r requirements.txt --user --upgrade
