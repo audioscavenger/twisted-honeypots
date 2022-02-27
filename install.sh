9ff0826bb01a25bdfe2a7bb0ea9785843679167a
Eric <dev@derewonko.com>
2022-02-26 22:52:37 -0700

    Update install.sh
    updated for 2022

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+export PATH=$PATH:/root/.local/bin
 
-apk --update --upgrade add git python3 py3-pip python3-dev curl sudo geoip libc-dev linux-headers libffi-dev openssl-dev
-
+apk --update --upgrade add git python3 py3-pip python3-dev curl sudo geoip libc-dev linux-headers libffi-dev openssl-dev mariadb-dev
+pip install --upgrade pip
 pip3 install -r requirements.txt --user --upgrade
