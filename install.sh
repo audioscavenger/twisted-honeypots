9e19bb6d01fc03788dface50969c8af774f00cf2
Eric <dev@derewonko.com>
2022-02-26 22:22:01 -0700

    Update install.sh
    updated for Alpine

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install geoip-bin -y
+apk --update --upgrade add git python3 py3-pip python3-dev curl sudo geoip libc-dev linux-headers libffi-dev openssl-dev
+
 pip3 install -r requirements.txt --user --upgrade
